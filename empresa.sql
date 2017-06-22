2cb5cfba01bdccd1b653033d5ff72599824641b7
Macarena Belén Garcia Arcija <belugarciaarcija@gmail.com>
2017-06-22 00:29:20 -0300

    base de datos

---
+++ empresa.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE empleados(
+    legajo integer NOT NULL,
+    dni integer NOT NULL,
+    apellido text NOT NULL,
+    nombre text NOT NULL,
+    fecha_nacimiento date NOT NULL,
+    genero character(1) NOT NULL,
+    domicilio text,
+    localidad text,
+    sector text,
+    puesto text
+);
+
+CREATE TABLE clientes(
+    dni integer NOT NULL,
+    apellido text NOT NULL,
+    nombre text NOT NULL,
+    fecha_nacimiento date NOT NULL,
+    genero character(1) NOT NULL,
+    domicilio text,
+    localidad text
+);
